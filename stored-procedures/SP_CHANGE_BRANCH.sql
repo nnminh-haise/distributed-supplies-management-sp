15a0479f86bb1295969c981f5fc1971cca367f81
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-04-04 21:10:21 +0700

    feat: add new sp and fix change branch sp

--- stored-procedures/SP_CHANGE_BRANCH.sql
+++ stored-procedures/SP_CHANGE_BRANCH.sql
@@ -1,70 +1,48 @@
 CREATE PROCEDURE SP_CHANGE_BRANCH
 	@MANV INT, 
 	@MACN nchar(10)
-AS
-    DECLARE @LGNAME VARCHAR(50)
-    DECLARE @USERNAME VARCHAR(50)
-    
+AS    
     SET XACT_ABORT ON;
-    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
-    
+    SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;  
 BEGIN
-	BEGIN DISTRIBUTED TRAN -- Bắt đầu transaction phân tán
+	-- * Bắt đầu transaction phân tán
+	BEGIN DISTRIBUTED TRAN
 		DECLARE @HONV NVARCHAR(40)
 		DECLARE @TENNV NVARCHAR(10)
 		DECLARE @DIACHINV NVARCHAR(100)
 		DECLARE @NGAYSINHNV DATETIME
 		DECLARE @LUONGNV FLOAT						
 		
-        -- Lấy thông tin nhân viên cũ để tiện cho việc kiểm tra.
+        -- * Lấy thông tin nhân viên cũ để tiện cho việc kiểm tra.
 		SELECT
             @HONV = HO,
             @TENNV = TEN,
+			@CMND = CMND,
             @DIACHINV = DIACHI,
             @NGAYSINHNV = NGAYSINH,
             @LUONGNV = LUONG
         FROM NhanVien WHERE MANV = @MANV
 
-        -- Kiểm tra ở site sẽ chuyển tới đã có nhân viên đó chưa? Nếu rồi thì đổi trạng thái, ngược lại sẽ thêm nhân viên vào.
-		IF EXISTS(SELECT MANV FROM LINK1.QLVT_DATHANG.dbo.NhanVien
-			        WHERE HO = @HONV
-                        AND TEN = @TENNV
-                        AND DIACHI = @DIACHINV
-                        AND NGAYSINH = @NGAYSINHNV
-                        AND LUONG = @LUONGNV)
+        -- * Kiểm tra ở site sẽ chuyển tới đã có nhân viên đó chưa?
+		-- * Nếu rồi thì đổi trạng thái, ngược lại sẽ thêm nhân viên vào.
+		IF EXISTS(SELECT MANV FROM LINK1.QLVT_DATHANG.dbo.NhanVien WHERE CMND = @CMND)
 		    BEGIN
 				UPDATE LINK1.QLVT_DATHANG.dbo.NhanVien
 				SET TrangThaiXoa = 0
-				WHERE MANV = (SELECT MANV FROM LINK1.QLVT_DATHANG.dbo.NhanVien
-								WHERE HO = @HONV
-                                    AND TEN = @TENNV
-                                    AND DIACHI = @DIACHINV
-									AND NGAYSINH = @NGAYSINHNV
-                                    AND LUONG = @LUONGNV)
+				WHERE MANV = (SELECT MANV FROM LINK1.QLVT_DATHANG.dbo.NhanVien WHERE CMND = @CMND)
 		    END
-		-- Chưa tồn tại nhân viên ở site sẽ chuyển đến thì ta sẽ thêm mới hoàn toàn vào chi nhánh mới với mã nhân viên mới sẽ theo công thức: MANV_MOI = Max(MANV) + 1
+		-- * Chưa tồn tại nhân viên ở site sẽ chuyển đến thì ta sẽ thêm mới 
+		-- * hoàn toàn vào chi nhánh mới với mã nhân viên mới sẽ theo công thức: MANV_MOI = Max(MANV) + 1
         ELSE 
 		    BEGIN
 			    INSERT INTO LINK1.QLVT_DATHANG.dbo.NhanVien
-                    (MANV, HO, TEN, DIACHI, NGAYSINH, LUONG, MACN, TRANGTHAIXOA)
+                    (MANV, CMND, HO, TEN, DIACHI, NGAYSINH, LUONG, MACN, TRANGTHAIXOA)
 			    VALUES (
-                    (SELECT MAX(MANV) FROM LINK0.QLVT_DATHANG.dbo.NhanVien) + 1, @HONV, @TENNV, @DIACHINV, @NGAYSINHNV, @LUONGNV, @MACN, 0)
+                    (SELECT MAX(MANV) FROM LINK0.QLVT_DATHANG.dbo.NhanVien) + 1, @CMND,
+					@HONV, @TENNV, @DIACHINV, @NGAYSINHNV, @LUONGNV, @MACN, 0)
 		    END
 		
         -- Cập nhật lại trạng thái xoá của record nhân viên ở site cũ (site hiện tại)
 		UPDATE dbo.NhanVien SET TrangThaiXoa = 1 WHERE MANV = @MANV
 	COMMIT TRAN; -- Kết thúc transaction phân tán
-
-	-- Lưu ý: sp_droplogin và sp_dropuser không thể được thực thi trong một giao tác do người dùng định nghĩa
-	
-    -- Kiểm tra xem Nhân viên đã có login chưa, nếu đã có rồi thì xóa login đó đi
-	IF EXISTS (SELECT SUSER_SNAME(sid) FROM sys.sysusers
-                WHERE name = CAST(@MANV AS NVARCHAR))
-		BEGIN
-			SET @LGNAME = CAST((SELECT SUSER_SNAME(sid) FROM sys.sysusers
-                                WHERE name = CAST(@MANV AS NVARCHAR)) AS VARCHAR(50))
-			SET @USERNAME = CAST(@MANV AS VARCHAR(50))
-			EXEC SP_DROPUSER @USERNAME;
-			EXEC SP_DROPLOGIN @LGNAME;
-		END	
 END
