67d8242b6805361c66649b98d364f5a793098b8b
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-04-05 21:00:24 +0700

    fix: update sp list all employee

--- stored-procedures/SP_LIST_ALL_EMPLOYEES.sql
+++ stored-procedures/SP_LIST_ALL_EMPLOYEES.sql
@@ -1,45 +1,9 @@
-CREATE PROCEDURE SP_LIST_ALL_EMPLOYEES
-    @ROLE NVARCHAR(8),
-    @CHINHANH NCHAR(3)
-AS
-BEGIN
-    IF @ROLE = 'CongTy'
-        BEGIN
-            DECLARE @REQUESTING_AT_CURRENT_BRANCH INT
-            IF (EXISTS(SELECT 1 FROM NHANVIEN AS NV WHERE NV.MACN = @CHINHANH))
-                SET @REQUESTING_AT_CURRENT_BRANCH = 1
-            ELSE
-                SET @REQUESTING_AT_CURRENT_BRANCH = 0
+CREATE PROCEDURE SP_LIST_ALL_EMPLOYEES AS BEGIN
+SELECT
+    *
+FROM
+    NHANVIEN
+WHERE
+    TRANGTHAIXOA = 0;
 
-            IF @REQUESTING_AT_CURRENT_BRANCH = 1
-                BEGIN
-                    SELECT
-                        NV.MANV AS MANV,
-                        NV.CMND AS CMND,
-                        HO + ' ' + TEN AS HO_TEN,
-                        MV.DIACHI AS DIACHI
-                    FROM NHANVIEN AS NV
-                    ORDER BY HO + ' ' + TEN AS HO_TEN
-                END
-            ELSE
-                BEGIN
-                    SELECT
-                        NV.MANV AS MANV,
-                        NV.CMND AS CMND,
-                        HO + ' ' + TEN AS HO_TEN,
-                        MV.DIACHI AS DIACHI
-                    FROM LINK1.QLVT_DATHANG.DBO.NHANVIEN AS NV
-                    ORDER BY HO + ' ' + TEN AS HO_TEN
-                END
-        END
-    ELSE IF @ROLE = 'ChiNhanh'
-        BEGIN
-            SELECT
-                NV.MANV AS MANV,
-                NV.CMND AS CMND,
-                HO + ' ' + TEN AS HO_TEN,
-                MV.DIACHI AS DIACHI
-            FROM NHANVIEN AS NV
-            ORDER BY HO + ' ' + TEN AS HO_TEN
-        END
 END
