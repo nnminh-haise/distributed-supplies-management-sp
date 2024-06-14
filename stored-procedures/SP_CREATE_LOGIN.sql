8b68fbe49f3ddb6f1629bc80f487bf7936a24e5c
levandung-it03 <levandung.it03@gmail.com>
2024-06-15 06:22:23 +0700

    Push All Changes by levandung

--- stored-procedures/SP_CREATE_LOGIN.sql
+++ stored-procedures/SP_CREATE_LOGIN.sql
@@ -1,29 +1,31 @@
-CREATE PROC SP_CREATE_LOGIN
-    @LOGIN_NAME VARCHAR(50),
+ATE PROC SP_CREATE_LOGIN
+	@LOGIN_NAME VARCHAR(50),
     @PASS VARCHAR(50),
     @USERNAME VARCHAR(50),
     @ROLE VARCHAR(50)
 AS
-  DECLARE @RET INT
-  EXEC @RET = SP_ADDLOGIN @LOGIN_NAME, @PASS, 'QLVT_DATHANG'
-  IF (@RET = 1)
-  BEGIN
-    RAISERROR ('Duplicated login name', 16,1)
-    RETURN
-  END
+BEGIN
+	DECLARE @RET INT
+	EXEC @RET = SP_ADDLOGIN @LOGIN_NAME, @PASS, 'QLVT_DATHANG'
+	IF (@RET = 1)
+	BEGIN
+		RAISERROR ('Duplicated login name', 16,1)
+		RETURN
+	END
 
-  EXEC @RET = SP_GRANTDBACCESS @LOGIN_NAME, @USERNAME
-  IF (@RET = 1)
-  BEGIN
-    EXEC SP_DROPLOGIN @LGNAME
-    RAISERROR ('Duplicated username', 16,2)
-    RETURN
-  END
+	EXEC @RET = SP_GRANTDBACCESS @LOGIN_NAME, @USERNAME
+	IF (@RET = 1)
+	BEGIN
+		EXEC SP_DROPLOGIN @LOGIN_NAME
+		RAISERROR ('Duplicated username', 16,2)
+		RETURN
+	END
 
-  EXEC sp_addrolemember @ROLE, @USERNAME
-  IF @ROLE = 'ADMIN'
-  BEGIN
-    EXEC sp_addsrvrolemember @LGNAME, 'SecurityAdmin'
-    EXEC sp_addsrvrolemember @LGNAME, 'DBCREATOR'
-    EXEC sp_addsrvrolemember @LGNAME, 'ProcessAdmin'
-  END
+	EXEC sp_addrolemember @ROLE, @USERNAME
+	IF @ROLE = 'ADMIN'
+	BEGIN
+		EXEC sp_addsrvrolemember @LOGIN_NAME, 'SecurityAdmin'
+		EXEC sp_addsrvrolemember @LOGIN_NAME, 'DBCREATOR'
+		EXEC sp_addsrvrolemember @LOGIN_NAME, 'ProcessAdmin'
+	END
+END
